7ea6789f9d6c40ef08b4c046cc4e0269f5239125
marumimumemo <sagaminumber3@yahoo.co.jp>
2020-01-04 16:24:01 +0900

    NavigationBar+UIAlertController

--- NavigationController+UIAlertControllerSample/ViewController.swift
+++ NavigationController+UIAlertControllerSample/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var backbutton:UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonAlert(sender: UIBarButtonItem) {
+        let alertController:UIAlertController =
+            UIAlertController(title:"入力の途中ですが、終了しますか？",
+                              message:"これまでに入力した内容は破棄されます。掲載期限まであとn日！",
+                              preferredStyle: .alert)
+        
+        
+        // OK
+        let defaultAction:UIAlertAction =
+            UIAlertAction(title: "破棄して終了",
+                          style: .default,
+                          handler:{
+                            (action:UIAlertAction!) -> Void in
+                            print("破棄して終了")
+            })
+        
+        // Cancel
+        let cancelAction:UIAlertAction =
+            UIAlertAction(title: "入力を続ける",
+                          style: .cancel,
+                          handler:{
+                            (action:UIAlertAction!) -> Void in
+                            print("入力を続ける")
+            })
+        
+        // action
+        alertController.addAction(cancelAction)
+        alertController.addAction(defaultAction)
+        
+        // UIAlertController
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
